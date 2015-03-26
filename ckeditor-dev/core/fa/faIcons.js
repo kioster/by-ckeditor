e728ca3902c02a691b7b7a8a876609d29191e501
kioster <kiostergoo@gmail.com>
2015-03-26 19:54:17 +0800

    upload again

---
+++ ckeditor-dev/core/fa/faIcons.js
@@ -0,0 +1,90 @@
+/**
+ * Created by LZJ on 2015/3/1.
+ */
+
+(function (CKEDITOR) {
+	CKEDITOR.faIcons = {
+
+		sourcearea : "fa-file-code-o",
+		save : "fa-save",
+		newpage : "fa-file-o",
+		preview : "fa-eye",
+		print : "fa-print",
+		templates : "fa-file-text",
+
+		cut :"fa-cut",
+		copy : "fa-copy",
+		paste : "fa-paste",
+		pastetext : "fa-font",
+		pastefromword : "fa-file-word-o",
+		undo : "fa-reply",
+		redo : "fa-share",
+
+		find : "fa-search",
+		replace : "fa-refresh",
+		selectall : "fa-th-large",
+
+		form : "fa-list-alt",
+		checkbox : "fa-check-square"+" margin1",
+		radio : "fa-dot-circle-o"+" margin1",
+		textfield : "fa-terminal",
+		textarea : "fa-edit",
+		select : "fa-caret-square-o-down",
+		button : "fa-plus-square",
+		imagebutton : "fa-play-circle",
+		hiddenfield : "fa-asterisk",
+
+		bold : "fa-bold",
+		italic : "fa-italic",
+		underline : "fa-underline",
+		strike : "fa-strikethrough",
+		subscript : "fa-subscript",
+		superscript : "fa-superscript",
+		removeformat : "fa-repeat",
+
+		numberedlist : "fa-list-ol",
+		bulletedlist : "fa-list-ul",
+
+		indent : "fa-indent",
+		outdent : "fa-outdent",
+
+		blockquote : "fa-quote-right",
+
+		div : "fa-cube",
+
+		justifyLeft : "fa-align-left",
+		justifyCenter : "fa-align-center",
+		justifyRight : "fa-align-right",
+		justifyBlock : "fa-align-justify",
+
+		bidiLtr : "fa-long-arrow-right",
+		bidiRtr : "fa-long-arrow-left",
+
+		language : "fa-language",
+
+		link : "fa-link",
+		unlink : "fa-unlink",
+		anchor : "fa-flag",
+
+		codesnippet : "fa-code",
+		image : "fa-picture-o",
+		falsh : "fa-toggle-right"+" margin1",
+		table : "fa-table",
+		horizontalrule : "fa-minus"+" margin2",
+		smiley : "fa-smile-o"+" margin1",
+		specialchar : "fa-at"+" margin2",
+		pagebreak : "fa-files-o",
+		iframe : "fa-globe",
+
+		textcolor : "fa-square-o",
+		bgcolor : "fa-square",
+
+		maximize : "fa-arrows-alt"+" margin1",
+		showblocks : "fa-th-list",
+
+		about : "fa-info"+" margin5"
+	};
+	/*for(var v in CKEDITOR.faIcons){
+		CKEDITOR.faIcons[v] = CKEDITOR.faIcons[v] + " fa";
+	}*/
+})(CKEDITOR);
